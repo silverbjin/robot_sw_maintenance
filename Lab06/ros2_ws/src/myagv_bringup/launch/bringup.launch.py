081dc8b9a9403ef1cc44513cb1c31946b03a2f20
Jinho Bae <silverbjin@gmail.com>
2026-09-26 01:47:32 +0900

    Lab06: add monitor module for release-v2

--- Lab06/ros2_ws/src/myagv_bringup/launch/bringup.launch.py
+++ Lab06/ros2_ws/src/myagv_bringup/launch/bringup.launch.py
@@ -10,4 +10,14 @@ def generate_launch_description():
         output='screen',
     )
 
-    return LaunchDescription([control_node])
+    monitor_node = Node(
+        package='myagv_monitor',
+        executable='monitor_node',
+        name='myagv_monitor_node',
+        output='screen',
+    )
+
+    return LaunchDescription([
+        control_node,
+        monitor_node,
+    ])
